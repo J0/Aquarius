78c12d18890fa1965502d15e8e7681315e1c923e
E-Liang Tan <eliang@eliangtan.com>
2018-05-19 22:39:49 +0800

    Implement some IPFS experiments and compile a short TODO list

--- client/src/index.js
+++ client/src/index.js
@@ -16,18 +16,41 @@ program.parse(process.argv);
 
 // TODO: Implement client
 
-const node = new IPFS({ EXPERIMENTAL: { pubsub: true } });
+// TODO: Configure IPFS to choose random port
+// TODO: Try to get p2p-circuit working
+// TODO: Figure out why only one message gets received
+
+const node = new IPFS({
+  EXPERIMENTAL: {
+    pubsub: true,
+    relay: {
+      enabled: true,
+      hop: { enabled: true, active: false },
+    },
+  },
+});
 
 node.once('ready', () => {
-  // console.log('READY', node, node.pubsub);
-  node.pubsub.subscribe(
-    'hello',
-    { discover: true },
-    (msg) => console.log('Got message', msg.from, msg.data.toString()),
-    (err) => {
-      if (err) console.error('Failed to subscribe with error', err);
-      else console.log('Subscribed');
-      node.pubsub.publish('hello', new Buffer('ONEYTHSEONTIHSOEIHSNTEOIHSNOEHISNOETIH'));
-    },
-  );
+  console.log('READY');
+
+  node.swarm
+    // .connect('/p2p-circuit/ipfs/QmRzGVxdY7MUAe3UhSXFPPT6T3Fqyas37GbFwUW4CjebZn')
+    .connect('/ip4/10.192.122.2/tcp/36049/ipfs/QmRzGVxdY7MUAe3UhSXFPPT6T3Fqyas37GbFwUW4CjebZn')
+    .then(() => console.log('Subscribed'));
+
+  node.pubsub
+    .subscribe('TOPIC', { discover: true }, (msg) => {
+      console.log('Got message', msg.from, msg.data.toString());
+    })
+    .then(() => console.log('Connected'));
 });
+
+setInterval(() => {
+  const msg = 'L$RPCGLRCG';
+  console.log('Publishing', msg);
+  node.pubsub.publish('TOPIC', new Buffer(msg));
+}, 4000);
+
+setInterval(() => {
+  node.pubsub.peers('TOPIC').then((s) => console.log('Pubsub peers', s));
+}, 3000);
