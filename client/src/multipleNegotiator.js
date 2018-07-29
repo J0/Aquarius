c015fdcf492b6145e8f6be843f17c404e3dc43c3
E-Liang Tan <eliang@eliangtan.com>
2018-07-29 14:03:18 +0800

    Implement multiple negotiation max duration

--- client/src/multipleNegotiator.js
+++ client/src/multipleNegotiator.js
@@ -2,7 +2,7 @@ import StateMachine from 'javascript-state-machine';
 import Negotiator from './negotiator';
 import { isNewRideMessage, beginNegotiationMessage, isBeginNegotiationMessage } from './models';
 
-// const SEARCH_MAX_DURATION = 20000; // Time to negotiate before selecting
+const SEARCH_MAX_DURATION = 20000; // Time to negotiate before selecting
 
 export default class MultipleNegotiator {
   constructor(gridChatroom) {
@@ -10,21 +10,42 @@ export default class MultipleNegotiator {
     this.gridChatroom.setOnMessageCallback(this.onMainChatroomMessage.bind(this));
 
     this.negotiators = {};
+    this.confirmedPrices = {};
     this.resetState();
   }
 
   resetState() {
+    const multiNegotiator = this;
     this.state = new StateMachine({
-      init: 'idle',
+      init: 'start',
       transitions: [
-        { name: 'startNegotiating', from: 'idle', to: 'negotiating' },
-        { name: 'confirm', from: 'negotiating', to: 'confirmed' },
-        // { name: 'startSelecting', from: 'negotiating', to: 'idle' }, // Nothing to select from
-        // { name: 'startSelecting', from: 'negotiating', to: 'selecting' },
+        { name: 'startNegotiating', from: 'start', to: 'negotiating' },
+        { name: 'startSelecting', from: 'negotiating', to: 'selecting' },
+        { name: 'nothingToSelect', from: 'selecting', to: 'fail' },
+        { name: 'cancel', from: 'selecting', to: 'fail' },
+        { name: 'confirm', from: 'selecting', to: 'confirmed' },
         // { name: 'select', from: 'selecting', to: 'awaitingConfirmation' },
         // { name: 'startSelecting', from: 'awaitingConfirmation', to: 'selecting' },
         // { name: 'confirm', from: 'awaitingConfirmation', to: 'confirmed' },
       ],
+      methods: {
+        onAfterStartSelecting() {
+          // Transition to fail state if there's nothing to be selected.
+          if (Object.keys(multiNegotiator.confirmedPrices).length === 0) {
+            // HACK: Transition in the next event loop, otherwise it'll crash
+            setTimeout(() => {
+              this.nothingToSelect();
+            }, 0);
+          } else {
+            // TODO: Remove this block when selection implemented
+            multiNegotiator.confirmedOther = Object.keys(multiNegotiator.confirmedPrices)[0];
+            setTimeout(async () => {
+              this.confirm();
+              await multiNegotiator.cancelAllNegotiations();
+            }, 0);
+          }
+        },
+      },
     });
   }
 
@@ -39,15 +60,19 @@ export default class MultipleNegotiator {
 
     this.state.startNegotiating();
 
+    setTimeout(() => {
+      this.state.startSelecting();
+    }, SEARCH_MAX_DURATION);
+
     return new Promise((resolve, reject) => {
-      const negotiator = this;
+      const multiNegotiator = this;
       this.state.observe({
-        onIdle() {
+        onFail() {
           console.log('Idling');
           reject(new Error('Negotiations failed'));
         },
         onConfirmed() {
-          resolve(negotiator.ridePrice);
+          resolve(multiNegotiator.confirmedPrices[multiNegotiator.confirmedOther]);
         },
       });
     });
@@ -57,6 +82,8 @@ export default class MultipleNegotiator {
     await this.cancelAllNegotiations();
     this.price = undefined;
     this.isDriver = undefined;
+    this.confirmedOther = undefined;
+    this.confirmedPrices = {};
     this.resetState();
   }
 
@@ -85,15 +112,10 @@ export default class MultipleNegotiator {
         .negotiate(this.isDriver)
         .then(async (a) => {
           console.log(`Negotiation successful! ${JSON.stringify(a)}`);
-          this.ridePrice = {
-            otherAddr,
-            price: a.price,
-          };
-          this.state.confirm();
-          await this.cancelAllNegotiations();
+          this.confirmedPrices[otherAddr] = { otherAddr, price: a.price };
         })
         .catch(async (e) => {
-          console.log(`Negotiation failed, ${e}`);
+          console.log(`Negotiation failed, ${JSON.stringify(e)}`);
           await negotiator.destroy();
         });
     }
