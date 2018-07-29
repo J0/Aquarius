28018dfa6602694d1a1acb8e521dab28f20ad02f
leeyjjoel <lee.yi.jie.joel@gmail.com>
2018-07-29 11:22:38 +0800

    Fix gridID for location

--- gridserver/index.js
+++ gridserver/index.js
@@ -1,39 +1,41 @@
-var fetch = require('node-fetch');
+const fetch = require('node-fetch');
 const express = require('express');
-const MAP_QUEST_API_KEY = ``;
+const MAP_QUEST_API_KEY = `oYKE5xifFEEdwXIbcwMSgOWZUTbsBk7A`;
 const STREETMAPS_WEBSITE = 'https://www.mapquestapi.com/geocoding/v1/reverse';
 const PORT = process.env.PORT || 3000;
 const GRIDID_PREFIX = 'AQUARIUS GRID ';
 const app = express();
 
-function gridIDForLocation(lat, long) {
+async function gridIDForLocation(lat, long) {
   const query = `?key=${MAP_QUEST_API_KEY}&location=${lat},${long}`;
   const url = STREETMAPS_WEBSITE + query;
   console.log('Retrieving grid for', lat, long);
   console.log('The overall url is', url);
-  var postalCode;
-  fetch(url)
+  let gridID;
+  await fetch(url)
     .then(function(response) {
       if (response.status !== 200) {
         console.log('Error, resource unable to be retreived. Status Code:' + response.status);
         return;
       }
-      return response.json().then(function(data) {
-        postalCode = data['results'][0]['locations'][0]['postalCode'];
-        console.log(postalCode);
-        return GRIDID_PREFIX.substring(0, 2);
-      });
+      return response.json();
+    })
+    .then(function(data) {
+      gridID = data['results'][0]['locations'][0]['postalCode'].substring(0, 2);
+      return gridID;
     })
     .catch(function(err) {
       console.error('Fetch Error:', err);
     });
+  return Promise.resolve(gridID);
 }
 
 app.get('/grid/id/:lat/:long', (req, res) => {
   const { lat, long } = req.params;
-  const gridID = gridIDForLocation(lat, long);
-  res.set('Content-Type', 'application/json');
-  res.send({ lat, long, grid_id: gridID });
+  const gridID = gridIDForLocation(lat, long).then((gridID) => {
+    res.set('Content-Type', 'application/json');
+    res.send({ lat, long, grid_id: GRIDID_PREFIX + gridID });
+  });
 });
 
 app.listen(PORT, () => console.log(`Grid server listening on port ${PORT}`));
